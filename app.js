2353f68fba2b2be2c1ca557f44a5645b26694867
sayandip paul <sayandip.web@gmail.com>
2026-05-27 06:27:29 +0530

    added production domain to cors

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:5174",
+
   "https://accelia-three.vercel.app",
+
+  "https://www.acceliaclinicalsolutions.com",
+  "https://acceliaclinicalsolutions.com",
+
   /\.vercel\.app$/,
 ];
 
