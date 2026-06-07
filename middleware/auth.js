62a44807a9817485484cba8552f1a3a178c5d9c7
sayandip paul <sayandip.web@gmail.com>
2026-06-08 00:01:03 +0530

    Fix auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,9 @@ const Admin = require("../models/Admin");
 
 module.exports = async (req, res, next) => {
   try {
+    console.log("JWT_SECRET loaded:", !!process.env.JWT_SECRET);
+    console.log("Auth header:", req.headers.authorization);
+
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) return res.status(401).json({ message: "No token" });
@@ -17,6 +20,7 @@ module.exports = async (req, res, next) => {
     req.admin = admin;
     next();
   } catch (err) {
+    console.log("Auth error:", err.message);
     res.status(401).json({ message: "Token invalid or expired" });
   }
 };
